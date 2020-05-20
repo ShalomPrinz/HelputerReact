71977f39788b441745aa7386ee4c3710f2f531f8
Shalom Prinz <shalom.prinz@gmail.com>
2020-05-20 20:33:17 +0300

    add Lessons component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Lessons from "./components/Lessons";
 
 function App() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Lessons />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Lessons.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Lessons(props) {
+  return <h1>Lessons</h1>;
+}
+
+export default Lessons;
