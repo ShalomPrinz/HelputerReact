43311d81950b69431181a81faca6627761262cf8
Shalom Prinz <shalom.prinz@gmail.com>
2020-06-02 00:58:53 +0300

    This is the outline of the Pilot component

--- src/components/Pilot.jsx
+++ src/components/Pilot.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Pilot(props) {
-  return <h1>Pilot</h1>;
+  const row = "row h1 justify-content-center";
+
+  return (
+    <div className="col">
+      <div className={row}>Progress bar</div>
+      <div className={row}>Now running</div>
+      <div className={row}>All lessons in this pilot</div>
+      <div className={row}>Next --- Previous</div>
+    </div>
+  );
 }
 
 export default Pilot;
