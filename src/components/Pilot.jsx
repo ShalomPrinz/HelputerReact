31d304a59c07accf66033b784e34fdc0185a8a23
Shalom Prinz <shalom.prinz@gmail.com>
2020-06-02 11:47:30 +0300

    Pilot shows the progress bar and subject of the pilot. It will also show more components next

--- src/components/Pilot.jsx
+++ src/components/Pilot.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import ProgressBar from "./common/ProgressBar";
+
+const row = "row h1 justify-content-center";
 
 function Pilot(props) {
-  const row = "row h1 justify-content-center";
+  const [progress, setProgress] = useState(0);
+  const { subject } = props.match.params;
 
   return (
     <div className="col">
-      <div className={row}>Progress bar</div>
+      <div className={row}>{subject}</div>
+      <ProgressBar progress={progress} />
       <div className={row}>Now running</div>
       <div className={row}>All lessons in this pilot</div>
       <div className={row}>Next --- Previous</div>
