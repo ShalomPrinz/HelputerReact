3f755b3ab81190706d5ee935c1674e3243b1e87f
Shalom Prinz <shalom.prinz@gmail.com>
2020-06-03 00:49:34 +0300

    clicking on ListItem will also mark it as active for 1 second

--- src/components/common/ListItem.jsx
+++ src/components/common/ListItem.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Popover from "./Popover";
 
-function ListItem({ autolist, className = "", handleSelect, item }) {
+function ListItem({ autolist, className = "", handleSelect = () => {}, item }) {
+  const [active, setActive] = useState(false);
+
+  const changeActive = (isActive) => {
+    const updatedActive = isActive ? false : " active";
+    setActive(updatedActive);
+    return updatedActive;
+  };
+
+  const activate = () => {
+    const result = changeActive(active);
+    setTimeout(() => changeActive(result), 1000);
+  };
+
+  const onClick = () => {
+    activate();
+    handleSelect();
+  };
+
   const li = (
     <li
-      className={`list-group-item clickable ${className}`}
-      onClick={handleSelect}
+      className={`list-group-item clickable ${active} ${className}`}
+      onClick={onClick}
     >
       <Popover item={item} />
     </li>
