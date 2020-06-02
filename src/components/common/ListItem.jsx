eef9abfa093284276cd72a81c21931d3fbe7cfd4
Shalom Prinz <shalom.prinz@gmail.com>
2020-06-02 19:48:59 +0300

    Refactor: ListItem

--- src/components/common/ListItem.jsx
+++ src/components/common/ListItem.jsx
@@ -2,28 +2,17 @@ import React from "react";
 import Popover from "./Popover";
 
 function ListItem({ autolist, className = "", handleSelect, item, key }) {
-  if (!autolist)
-    return (
-      <li
-        key={key}
-        className={`list-group-item clickable ${className}`}
-        onClick={handleSelect}
-      >
-        <Popover item={item} />
-      </li>
-    );
-  else
-    return (
-      <ul className="list-group">
-        <li
-          key={key}
-          className={`list-group-item clickable ${className}`}
-          onClick={handleSelect}
-        >
-          <Popover item={item} />
-        </li>
-      </ul>
-    );
+  const li = (
+    <li
+      key={key}
+      className={`list-group-item clickable ${className}`}
+      onClick={handleSelect}
+    >
+      <Popover item={item} />
+    </li>
+  );
+
+  return autolist ? <ul className="list-group">{li}</ul> : li;
 }
 
 export default ListItem;
