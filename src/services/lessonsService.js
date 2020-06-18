b4475ffe069bc342a7b0d392cfd5732511ef2a05
Shalom Prinz <shalom.prinz@gmail.com>
2020-06-18 20:14:34 +0300

    added lessons.json - another json data file, containing all the lessons in the first Word tab

--- src/services/lessonsService.js
+++ src/services/lessonsService.js
@@ -2,120 +2,18 @@ import { randomProperty } from "../utils/object";
 import { shuffle } from "../utils/array";
 import hotkeys from "./hotkeysService";
 
-const lessonsLists = {
-  "עיצוב טקסט": [
-    {
-      id: 1,
-      name: "גודל גופן",
-      type: "ComboBox",
-    },
-    {
-      id: 2,
-      name: "הגדל גופן",
-      type: "Button",
-    },
-    {
-      id: 3,
-      name: "כווץ גופן",
-      type: "Button",
-    },
-    {
-      id: 4,
-      name: "גופן",
-      type: "ComboBox",
-    },
-    {
-      id: 5,
-      name: "מודגש",
-      type: "Button",
-    },
-    {
-      id: 6,
-      name: "נטוי",
-      type: "Button",
-    },
-    {
-      id: 7,
-      name: "קו תחתון",
-      type: "SplitButton",
-    },
-  ],
-  "פעולות בסיסיות": [
-    {
-      id: 1,
-      name: "ישר לימין",
-      type: "Button",
-    },
-    {
-      id: 2,
-      name: "מרכז",
-      type: "Button",
-    },
-    {
-      id: 3,
-      name: "ישר לשמאל",
-      type: "Button",
-    },
-    {
-      id: 4,
-      name: "ישר לשני הצדדים",
-      type: "Button",
-    },
-    {
-      id: 5,
-      name: "העתק",
-      type: "Button",
-    },
-    {
-      id: 6,
-      name: "הדבק",
-      type: "SplitButton",
-    },
-    {
-      id: 7,
-      name: "גזור",
-      type: "Button",
-    },
-    {
-      id: 8,
-      name: "בטל",
-      regex: "^בטל|אין אפשרות לבטל",
-      type: "SplitButton",
-    },
-  ],
-  "עיצוב בצבע": [
-    {
-      id: 1,
-      name: "צבע גופן",
-      type: "SplitButton",
-    },
-    {
-      id: 2,
-      name: "צבע סימון טקסט",
-      type: "SplitButton",
-    },
-    {
-      id: 3,
-      name: "מברשת עיצוב",
-      type: "Button",
-    },
-    {
-      id: 4,
-      name: "הצללה",
-      type: "SplitButton",
-    },
-    {
-      id: 5,
-      name: "אפקטי טקסט וטיפוגרפיה",
-      type: "MenuItem",
-    },
-  ],
-};
+const lessonsLists = require("../data/lessons.json");
 
 const topicDictionary = {
   TextDesign: "עיצוב טקסט",
   PageBasics: "פעולות בסיסיות",
   BasicDesign: "עיצוב בצבע",
+  Listing: "רשימה",
+  Headline: "כותרת",
+  DocumentManagement: "ניהול מסמך",
+  AdvancedText: "טקסט מתקדם",
+  ParagraphDesign: "עיצוב פסקאות",
+  AdvancedEditing: "עריכה מתקדמת",
 };
 
 export const randomLesson = () => {
